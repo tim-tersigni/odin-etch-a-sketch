0fb5b4a3bb7b277dc81992e8751cefa0c6bc7087
Tim Tersigni <tim.tersigni@gmail.com>
2022-05-16 01:54:44 -0500

    Make new button create new sketch grid from user input

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-function makeGridItems() {
+// add pixel divs to grid
+function makeGridItems(dim) {
   const container = document.querySelector('.grid-container');
-  for (let i = 0; i < 256; i++) {
+  for (let i = 0; i < dim*dim; i++) {
     const div = document.createElement('div');
     div.className = 'pixel-div';
     div.style.backgroundColor = '#f6f7f2';
-    div.style.width = '32px';
-    div.style.height = '32px';
 
     // hover listener
     div.addEventListener('mouseover', () => {
@@ -23,24 +22,48 @@ function clearGrid() {
   })
 }
 
-function newGrid(dim) {
+function setGridDimension(dim) {
   const container = document.querySelector('.grid-container');
   container.style.gridTemplateColumns = `repeat(${dim}, 1fr)`;
-  container.style.gridTemplateRows = `repeat(${dim}, 1fr);`
+  container.style.gridTemplateRows = `repeat(${dim}, 1fr)`;
 }
 
+// add listeners to menu buttons
 function menuItemListeners() {
   const clearButton = document.querySelector('.clear-button');
-  const newButton = document.querySelector('new-button');
+  const newButton = document.querySelector('.new-button');
 
   clearButton.addEventListener('click', () => {
     clearGrid();
-  })
+  });
 
-  // newButton.addEventListener('click', () => {
-
-  // })
+  newButton.addEventListener('click', () => {
+    const dim = dimensionInput();
+    clearGrid();
+    setGridDimension(dim);
+    makeGridItems(dim);
+  });
 }
 
-menuItemListeners();
-makeGridItems();
+// prompt the user for sketch dimension and return it
+function dimensionInput() {
+  let dim
+  while(typeof(dim) != 'number') {
+    dim = prompt('Enter sketch dimension (single number)')
+    if(typeof(dim) == 'string'){
+      dim = parseInt(dim, 10);
+    }
+  }
+  return dim;
+}
+
+window.addEventListener('load', () => {
+  // set menu button listeners
+  menuItemListeners();
+
+  // get sketch dimension number from input
+  const dim = dimensionInput();
+
+setGridDimension(dim);
+makeGridItems(dim);
+});
