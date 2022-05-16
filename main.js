2b6ff9f98076d7bd027ce7a847c608d417809413
Tim Tersigni <tim.tersigni@gmail.com>
2022-05-16 01:21:55 -0500

    Make clear button clear the grid

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ function makeGridItems() {
   const container = document.querySelector('.grid-container');
   for (let i = 0; i < 256; i++) {
     const div = document.createElement('div');
+    div.className = 'pixel-div';
     div.style.backgroundColor = '#f6f7f2';
     div.style.width = '32px';
     div.style.height = '32px';
@@ -15,4 +16,31 @@ function makeGridItems() {
   }
 }
 
+function clearGrid() {
+  const pixels = document.querySelectorAll('.pixel-div')
+  pixels.forEach(pixel => {
+    pixel.style.backgroundColor = '#f6f7f2';
+  })
+}
+
+function newGrid(dim) {
+  const container = document.querySelector('.grid-container');
+  container.style.gridTemplateColumns = `repeat(${dim}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${dim}, 1fr);`
+}
+
+function menuItemListeners() {
+  const clearButton = document.querySelector('.clear-button');
+  const newButton = document.querySelector('new-button');
+
+  clearButton.addEventListener('click', () => {
+    clearGrid();
+  })
+
+  // newButton.addEventListener('click', () => {
+
+  // })
+}
+
+menuItemListeners();
 makeGridItems();
