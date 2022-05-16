972d5cb3b85255416a322e4cb55f381f8812700e
Tim Tersigni <tim.tersigni@gmail.com>
2022-05-16 01:00:47 -0500

    Add buttons to menu

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ function makeGridItems() {
     div.style.backgroundColor = '#f6f7f2';
     div.style.width = '32px';
     div.style.height = '32px';
+
+    // hover listener
+    div.addEventListener('mouseover', () => {
+      div.style.backgroundColor = '#121619'
+    });
+
     container.appendChild(div);
   }
 }
